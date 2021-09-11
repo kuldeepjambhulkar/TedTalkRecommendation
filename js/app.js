0ffb40d24a145b475be645ac104f8c677133af02
Kuldeep Jambhulkar <89466207+kuldeepjambhulkar@users.noreply.github.com>
2021-09-11 19:52:48 +0530

    Style Fix card style & font

--- js/app.js
+++ js/app.js
@@ -5,14 +5,16 @@ let talkDB = {
             link:"https://www.ted.com/talks/wendy_de_la_rosa_10_steps_to_boost_your_financial_health_that_you_can_do_in_a_day",
             speaker:"Zendeya",
             length:"12:10",
-            thumbnail:"https://pi.tedcdn.com/r/talkstar-photos.s3.amazonaws.com/uploads/8f011d01-08d7-4864-81d5-207a4c47cfbc/YourMoneyYourMind_2021V_E08-embed.jpg?quality=89&w=320"
+            thumbnail:"https://pi.tedcdn.com/r/talkstar-photos.s3.amazonaws.com/uploads/8f011d01-08d7-4864-81d5-207a4c47cfbc/YourMoneyYourMind_2021V_E08-embed.jpg?quality=89&w=320",
+            objectPosition:"left"
         },
         {
             name:"Why it's so hard to make healthy decisions",
             link:"https://www.ted.com/talks/david_asch_why_it_s_so_hard_to_make_healthy_decisions/up-next",
             speaker:"Peter",
             length:"12:10",
-            thumbnail:"https://pi.tedcdn.com/r/talkstar-photos.s3.amazonaws.com/uploads/901cbfdb-61cb-4b3a-bf67-21b849c386bc/Jim+Tamm.jpeg?quality=89&w=320"
+            thumbnail:"https://pi.tedcdn.com/r/talkstar-photos.s3.amazonaws.com/uploads/901cbfdb-61cb-4b3a-bf67-21b849c386bc/Jim+Tamm.jpeg?quality=89&w=320",
+            objectPosition:"left"
         }
     ],
     "Biomimicry":[
@@ -21,14 +23,16 @@ let talkDB = {
             link:"https://www.ted.com/talks/jaap_de_roode_how_butterflies_self_medicate/up-next",
             speaker:"Gobblin",
             length:"12:10",
-            thumbnail:"https://pi.tedcdn.com/r/pe.tedcdn.com/images/ted/bfd9596723d0142dc573c04fd56c2ecbacd21b7f_2880x1620.jpg?quality=89&w=320"
+            thumbnail:"https://pi.tedcdn.com/r/pe.tedcdn.com/images/ted/bfd9596723d0142dc573c04fd56c2ecbacd21b7f_2880x1620.jpg?quality=89&w=320",
+            objectPosition:"left"
         },
         {
             name: "What humans can learn from semi-intelligent slime", 
             link:"https://www.ted.com/talks/heather_barnett_what_humans_can_learn_from_semi_intelligent_slime/up-next",
             speaker:"Tony",
             length:"12:10",
-            thumbnail:"https://pi.tedcdn.com/r/talkstar-photos.s3.amazonaws.com/uploads/8f011d01-08d7-4864-81d5-207a4c47cfbc/YourMoneyYourMind_2021V_E08-embed.jpg?quality=89&w=320"
+            thumbnail:"https://pi.tedcdn.com/r/talkstar-photos.s3.amazonaws.com/uploads/8f011d01-08d7-4864-81d5-207a4c47cfbc/YourMoneyYourMind_2021V_E08-embed.jpg?quality=89&w=320",
+            objectPosition:"left"
         }
     ],
 }
@@ -70,6 +74,11 @@ function expandTopic(e){
 
         let thumbnail = document.createElement('img');
         thumbnail.setAttribute('src', topicArray[i].thumbnail);
+
+        // if((window.matchMedia("(max-width: 450px)").matches)){
+        // thumbnail.style.objectPosition = topicArray[i].objectPosition;
+        // }
+
         
         let a = document.createElement('a');
         a.setAttribute('href', topicArray[i].link);
